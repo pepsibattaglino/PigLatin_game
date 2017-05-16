178b5e8df46580f6ed2e1bb98a5688fd7124f2cf
Giuseppe Battaglino <pepsibattaglino@gmail.com>
2017-05-16 02:45:14 -0300

    correção comentários

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ pyg = "ay"
 original = input("Enter a word:")
 
 if len(original) > 0 and original.isalpha():    # nesse if verificamos se a palavra informada nao eh vazia e se nao contem numeros
-    word = original.lower()                     # aqui mudamos a letras para minusculas
+    word = original.lower()                     # aqui mudamos as letras da palavra para minusculas
     first = word[0]                             # aqui armazenamos a primeira letra da palavra
     new_word = word[1:len(word)] + first + pyg  # aqui armazenamos o resultado final (palavra sem a primeira letra + primeira letra + "ay")
     print(new_word)
